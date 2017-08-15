a3b2adab9c4a36ff824079abaeecf0532950a597
zhubinge <zhubinge@mail.xusenintl.com>
2017-08-15 17:21:55 +0800

    path匹配

--- src/components/static/MainNav.vue
+++ src/components/static/MainNav.vue
@@ -1,28 +1,29 @@
 <template>
     <tabbar slot="bottom">
         <tabbar-item link='/home' :selected='isHome'>
-            <span v-bind:class='[isHome?"icon-sy-index-on":"icon-sy-index"]' slot='icon'></span>
+            <span :class='[isHome?"icon-sy-index-on":"icon-sy-index"]' slot='icon'></span>
             <span slot='label'>首页</span>
         </tabbar-item>
         <tabbar-item link='/loan' :selected='isLoan'>
-            <span v-bind:class='[isLoan?"icon-sy-loan-on":"icon-sy-loan"]' slot='icon'></span>
+            <span :class='[isLoan?"icon-sy-loan-on":"icon-sy-loan"]' slot='icon'></span>
             <span slot='label'>借款</span>
         </tabbar-item>
         <tabbar-item link='/find' :selected='isFind'>
-            <span v-bind:class='[isFind?"icon-sy-find-on":"icon-sy-find"]' slot='icon'></span>
+            <span :class='[isFind?"icon-sy-find-on":"icon-sy-find"]' slot='icon'></span>
             <span slot='label'>发现</span>
         </tabbar-item>
         <tabbar-item link='/user' :selected='isUser'>
-            <span v-bind:class='[isUser?"icon-sy-my-on":"icon-sy-my"]' slot='icon'></span>
+            <span :class='[isUser?"icon-sy-my-on":"icon-sy-my"]' slot='icon'></span>
             <span slot='label'>我的</span>
         </tabbar-item>
     </tabbar>
 </template>
 <script>
 export default {
+    name: 'main-nav',
     computed: {
         isHome() {
-            if (this.$route.path === '/home') {
+            if (this.$route.path === '/home' || this.$route.path === '/') {
                 return true
             }
         },
