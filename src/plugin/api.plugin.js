d3d760ea0d4786be5f08852a5db336b6155c2832
wyf <393192789@qq.com>
2017-08-15 16:06:24 +0800

    bj

--- src/plugin/api.plugin.js
+++ src/plugin/api.plugin.js
@@ -15,7 +15,7 @@ export default {
         axios.defaults.headers['client'] = '5'
 
         Vue.prototype.api = {
-            ajax: function(apiKey, option) {
+            ajax: function (apiKey, option) {
                 // console.log(111, this.getApi)
                 let _api = this.getApi(apiKey).split('@')
                 // let res
@@ -38,7 +38,7 @@ export default {
                     return error
                 })
             },
-            getApi: function(key) {
+            getApi: function (key) {
                 let prx = environment.prefix
                 let _api = Api[key]
                 if (!/\/$/.test(prx) && !/^\//.test(_api)) {
@@ -46,8 +46,82 @@ export default {
                 }
                 return prx + _api
             },
-            handleError: function(error) {
+            handleError: function (error) {
                 return Promise.reject(error.message || error)
+            },
+            getUrlParam: function (name, _location) {
+                let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)') // 构造一个含有目标参数的正则表达式对象
+                let r
+
+                if (_location) {
+                    let loc = _location.split('?')
+                    if (loc.length > 1) {
+                        _location = loc[1]
+                    } else {
+                        return null
+                    }
+                    r = _location.match(reg) // 匹配目标参数
+                } else if (location.search) {
+                    r = location.search
+                        .substr(1)
+                        .match(reg) // 匹配目标参数
+                } else {
+                    return null
+                }
+
+                if (r != null) {
+                    return window['unescape'](r[2])
+                }
+                return null // 返回参数值
+            },
+            storage: {
+                getLS: function (key) {
+                    var val = this.deserializeLS(localStorage.getItem(key))
+                    return (val === undefined ? null : val)
+                },
+                setLS: function (key, value) {
+                    if (this.getLS(key) !== null) {
+                        this.removeLS(key)
+                    }
+                    localStorage.setItem(key, this.serializeLS(value))
+                },
+                removeLS: function (key) {
+                    if (!key) {
+                        localStorage.clear()
+                    } else {
+                        localStorage.removeItem(key)
+                    }
+                },
+                getSS: function (key) {
+                    var val = this.deserializeLS(sessionStorage.getItem(key))
+                    return (val === undefined ? null : val)
+                },
+                setSS: function (key, value) {
+                    if (this.getSS(key) !== null) {
+                        this.removeSS(key)
+                    }
+                    sessionStorage.setItem(key, this.serializeLS(value))
+                },
+                removeSS: function (key) {
+                    if (!key) {
+                        sessionStorage.clear()
+                    } else {
+                        sessionStorage.removeItem(key)
+                    }
+                },
+                serializeLS: function (value) {
+                    if (typeof value === 'string') {
+                        return value
+                    }
+                    return JSON.stringify(value)
+                },
+                deserializeLS: function (value) {
+                    try {
+                        return JSON.parse(value)
+                    } catch (e) {
+                        return value || undefined
+                    }
+                }
             }
         }
     }
